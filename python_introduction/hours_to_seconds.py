568994ff4a4a344d86f539d52ec74b8cbc6eb17e
Kojo Quao <quaogeorge@icloud.com>
2025-09-16 13:49:11 +0000

    hour to seconds script

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,12 @@
+# hours_to_seconds
+# Define the number of hours
+hours = 2
+
+# Conversion factor: 1 hour = 3600 seconds
+seconds_in_hour = 3600
+
+# Calculate total seconds
+seconds = hours * seconds_in_hour
+
+# Print result
+print(f"{hours} hour(s) is {seconds} seconds.")
