cb4555388aa3eb31e911798eb559cd207ea27339
Kojo Quao <quaogeorge@icloud.com>
2025-10-03 20:43:40 +0000

    Simple Cal Script Code update

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,5 +1,3 @@
-# programming_paradigm/test_simple_calculator.py
-
 import unittest
 from simple_calculator import SimpleCalculator
 
@@ -9,53 +7,35 @@ class TestSimpleCalculator(unittest.TestCase):
         """Set up a new calculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    # --- Addition Tests ---
-    def test_addition_with_positive_numbers(self):
+    # --- Required Addition Test ---
+    def test_addition(self):
+        """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
-
-    def test_addition_with_negative_numbers(self):
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 10), 10)
         self.assertEqual(self.calc.add(-2, -3), -5)
 
-    def test_addition_with_mixed_signs(self):
-        self.assertEqual(self.calc.add(-2, 5), 3)
-
-    def test_addition_with_zero(self):
-        self.assertEqual(self.calc.add(0, 10), 10)
-
     # --- Subtraction Tests ---
-    def test_subtraction_with_positive_numbers(self):
+    def test_subtraction(self):
+        """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(10, 5), 5)
-
-    def test_subtraction_with_negative_numbers(self):
         self.assertEqual(self.calc.subtract(-5, -2), -3)
-
-    def test_subtraction_with_mixed_signs(self):
         self.assertEqual(self.calc.subtract(5, -2), 7)
-
-    def test_subtraction_resulting_in_negative(self):
         self.assertEqual(self.calc.subtract(2, 5), -3)
 
     # --- Multiplication Tests ---
-    def test_multiplication_with_positive_numbers(self):
+    def test_multiplication(self):
+        """Test the multiplication method."""
         self.assertEqual(self.calc.multiply(4, 5), 20)
-
-    def test_multiplication_with_negative_numbers(self):
         self.assertEqual(self.calc.multiply(-4, 5), -20)
-
-    def test_multiplication_with_zero(self):
         self.assertEqual(self.calc.multiply(7, 0), 0)
 
     # --- Division Tests ---
-    def test_division_with_positive_numbers(self):
+    def test_division(self):
+        """Test the division method."""
         self.assertEqual(self.calc.divide(10, 2), 5)
-
-    def test_division_with_negative_numbers(self):
         self.assertEqual(self.calc.divide(-10, 2), -5)
-
-    def test_division_with_floats(self):
         self.assertAlmostEqual(self.calc.divide(7, 2), 3.5)
-
-    def test_division_by_zero(self):
         self.assertIsNone(self.calc.divide(10, 0))
 
 if __name__ == "__main__":
